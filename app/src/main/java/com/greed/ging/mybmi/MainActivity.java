08525269d5e711777f15bb6f173643a6af2c13e5
ging7 <g776808@gmail.com>
2018-03-28 21:31:15 +0800

    取得UI元件，程式撰寫計算bmi

--- app/src/main/java/com/greed/ging/mybmi/MainActivity.java
+++ app/src/main/java/com/greed/ging/mybmi/MainActivity.java
@@ -2,12 +2,39 @@ package com.greed.ging.mybmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edWeight;
+    private EditText edHeight;
+    private Button bHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+
+    }
+
+    private void findViews() {
+        edWeight = findViewById(R.id.ed_weight);
+        edHeight = findViewById(R.id.ed_height);
+        bHelp = findViewById(R.id.b_help);
+    }
+
+    public void bmi(View v){
+        //取得元件值並計算BMI
+        String h = edHeight.getText().toString();
+        String w = edWeight.getText().toString();
+        float weight = Float.parseFloat(w); //強制轉型
+        float height = Float.parseFloat(h);
+        float bmi = weight/(height*height);
+        Log.d("BMI", String.valueOf(bmi));
     }
 }
